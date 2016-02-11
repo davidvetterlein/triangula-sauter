aecec12002158f3d918e1f81ff779afc96323039
David Vetterlein <davidvtterlein@gmail.com>
2016-02-11 21:29:28 +0100

    hook

---
+++ Assets/scripte/TheHook.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TheHook : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        CallHook();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        
+    }
+
+    public void CallHook()
+    {
+        gameObject.GetComponent<DistanceJoint2D>().enabled = true;
+        RaycastHit2D hit = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y + 1), Vector2.up);
+        if (hit.collider != null)
+        {
+            gameObject.GetComponent<DistanceJoint2D>().connectedAnchor = hit.collider.transform.position;
+        }
+    }
+
+    public void UndoCall()
+    {
+        gameObject.GetComponent<DistanceJoint2D>().enabled = false;
+    }
+}
