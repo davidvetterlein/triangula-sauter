ff1bb68edd51f11b2685515cac7a2d6737dadfd1
David V <davidvtterlerlein@gmail.com>
2016-03-01 12:37:45 +0100

    lbzhghgg

---
+++ Assets/scripte/jumper.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class jumper : MonoBehaviour {
+
+	public float up;
+	float maxy;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RaycastHit2D hit = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y - 0.5f), Vector2.up);
+		if (hit.collider.tag == "Player")
+        {
+            hit.collider.gameObject.GetComponent<Rigidbody2D>().Sleep();
+			if(hit.collider.gameObject.transform.position.y <= maxy){
+				hit.collider.gameObject.transform.position += new Vector3(0, up, 0);
+			}
+        } else{
+			hit.collider.gameObject.GetComponent<Rigidbody2D>().WakeUp();
+		}
+	}
+}
---
+++ GegnerKi.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GegnerKi : MonoBehaviour {
+	public Vector2 Point1;
+	public Vector2 Point2;
+	public float speed;
+	int jump = 50;
+	public int newJump = 50;
+	public float up;
+	bool zupunkt1 = false;
+	public int wahrscheinlichkeit = 0;
+	bool grounded = true;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int rand;
+		rand = Random.Range(0,1000);
+		if(rand >= wahrscheinlichkeit && grounded == true){
+			jump = newJump;
+		}
+		if(jump > 0){
+			jump -= 1;
+			gameObject.GetComponent<Rigidbody2D>().Sleep();
+			gameObject.transform.position += new Vector2 (0, up);
+		}else{
+			gameObject.GetComponent<Rigidbody2D>().WakeUp();
+		}
+		
+		float step = speed * Time.deltaTime;
+		if(zupunkt1 == true){
+			transform.MoveTowards(gameObject.transform.position, Point1, step);
+		}else{
+			transform.MoveTowards(gameObject.transform.position, Point2, step);
+		}
+		if(gameObject.transform.position == Point1){
+			zupunkt1 = false;
+		}
+		if(gameObject.transform.position == Point2){
+			zupunkt1 = true;
+		}
+	}
+	
+	void OnCollisionEnter2D(Collision2D col){
+		grounded = true;
+	}
+}
