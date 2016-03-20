75ead26950e52ee1732a8a4e27350f549595fcf7
David Vetterlein <davidvtterlein@gmail.com>
2016-03-20 21:25:12 +0100

    Mobil&Skill

--- Assets/scripte/Skillsystem.cs
+++ Assets/scripte/Skillsystem.cs
@@ -9,51 +9,73 @@ public class Skillsystem : MonoBehaviour {
 	public int[] JumpLevel;
 	public int Jumplength;
 
-    public Text Xptext;
+    public Slider Xpslider;
     public int Xp;
-    public int[] Xppreise;
+    public int Punkte;
+    public Text Punktetxt;
 
     public GameObject skillsystem;
-    public Text CJL;
-    public Text NJL;
-    public Text JS;
+    public GameObject IngameButtons;
+
+    public Text JumpLv;
+    public Text SpeedLv;
 
 
     // Use this for initialization
     void Start () {
-		Levelspeed = PlayerPrefs.GetInt ("LevelSpeed");
+		Levelspeed = PlayerPrefs.GetInt ("Levelspeed");
 		Jumplength = PlayerPrefs.GetInt ("JumpLength");
+        Punkte = PlayerPrefs.GetInt("Punkte");
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        Punktetxt.text = Punkte.ToString();
         Xp = gameObject.GetComponent<steuerung>().Xp;
-        Xptext.text = Xp.ToString();
+        Xpslider.value = Xp;
+        if(Xp >= 100)
+        {
+            Punkte += 1;
+            gameObject.GetComponent<steuerung>().Xp = 0;
+        }
+
 		gameObject.GetComponent<steuerung> ().newjump = JumpLevel [Jumplength];
         gameObject.GetComponent<steuerung>().speed = SpeedLevel[Levelspeed];
-        NJL.text = Xppreise[Jumplength].ToString();
-        JS.text = JumpLevel[Jumplength].ToString();
-        int cjl = Jumplength +1;
-        CJL.text = cjl.ToString();
+
+        JumpLv.text = JumpLevel[Jumplength].ToString();
+        SpeedLv.text = SpeedLevel[Levelspeed].ToString();
+
         PlayerPrefs.SetInt("JumpLength", Jumplength);
+        PlayerPrefs.SetInt("Levelspeed", Levelspeed);
+        PlayerPrefs.SetInt("Punkte", Punkte);
 
         if (Input.GetKeyDown("i")){
             if(skillsystem.activeSelf == true)
             {
                 skillsystem.SetActive(false);
-            }else{
+                IngameButtons.SetActive(true);
+            }
+            else{
                 skillsystem.SetActive(true);
+                IngameButtons.SetActive(false);
             }
         }
     }
 
     public void UpJumpLv()
     {
-        if(Xp >= Xppreise[Jumplength])
+        if(Punkte >= 1 && Jumplength < JumpLevel.Length -1)
         {
-            gameObject.GetComponent<steuerung>().Xp -= Xppreise[Jumplength];
+            Punkte -= 1;
             Jumplength += 1;
-            skillsystem.SetActive(false);
+        }
+    }
+    public void UpSpeedLv()
+    {
+        if (Punkte >= 1 && Levelspeed < SpeedLevel.Length -1)
+        {
+            Punkte -= 1;
+            Levelspeed += 1;
         }
     }
 
@@ -62,9 +84,11 @@ public class Skillsystem : MonoBehaviour {
         if (skillsystem.activeSelf == true)
         {
             skillsystem.SetActive(false);
+            IngameButtons.SetActive(true);
         }
         else {
             skillsystem.SetActive(true);
+            IngameButtons.SetActive(false);
         }
     }
 }
